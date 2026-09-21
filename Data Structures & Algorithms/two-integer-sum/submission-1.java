743629cf07e40066e7ecc9f48419089d579b2743
Tamil Ramasamy <trmsmy@users.noreply.github.com>
2026-09-21 19:10:58 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> seen = new HashMap<>();
+        for(int i=0; i< nums.length; i++) {
+
+            int diff = target - nums[i];
+
+            if(seen.containsKey(diff)) {
+                return new int[]{seen.get(diff), i};
+            }
+
+            seen.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("no pairs found");
+
+    }
+}
